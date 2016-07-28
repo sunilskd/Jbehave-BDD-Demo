181a85935912b5bc7b88c515f8e73aaf22d822d0
Gajendra Sahu <gajendra.sahu@bankersaccuity.com>
2016-07-28 17:19:04 -0500

    started-refactoring-snapshot-comparision

--- src/main/java/org/web/kyc/comparator/Comparator.java
+++ src/main/java/org/web/kyc/comparator/Comparator.java
@@ -1,9 +1,11 @@
 package org.web.kyc.comparator;
 import org.im4java.core.CompareCmd;
+import org.im4java.core.ConvertCmd;
+import org.im4java.core.IM4JavaException;
 import org.im4java.process.StandardStream;
 import org.im4java.core.IMOperation;
-import org.jbehave.web.selenium.WebDriverPage;
-import org.jbehave.web.selenium.WebDriverProvider;
+
+import java.io.IOException;
 
 import static org.web.kyc.jbehave.pages.WebDriverUtils.readProperties;
 
@@ -13,33 +15,55 @@ public class Comparator {
 
     public static boolean compareImages(String exp, String act, String diff){
 
+        convertImage(exp);
+        convertImage(act);
+
         // This instance wraps the compare command
         CompareCmd compare = new CompareCmd();
 
         // For metric-output
         compare.setErrorConsumer(StandardStream.STDERR);
-        IMOperation cmpOp = new IMOperation();
+        IMOperation compareOp = new IMOperation();
 
         // Set the compare metric
-        cmpOp.metric("mae");
+        compareOp.metric("mae");
 
         // Add the expected image
-        cmpOp.addImage(exp);
+        compareOp.addImage(exp);
 
         // Add the current image
-        cmpOp.addImage(act);
+        compareOp.addImage(act);
 
         // This stores the difference
-        cmpOp.addImage(diff);
+        compareOp.addImage(diff);
 
         try {
             // Do the compare
             compare.setSearchPath(readProperties().getImageMagicPath());
-            compare.run(cmpOp);
+            compare.run(compareOp);
             return true;
         }
         catch (Exception ex) {
             return false;
         }
     }
+
+    public static void convertImage(String exp){
+        ConvertCmd convert = new ConvertCmd();
+        convert.setSearchPath(readProperties().getImageMagicPath());
+        IMOperation convertOp = new IMOperation();
+        convertOp.addImage(exp);
+        convertOp.resize(1900, 1900);
+        convertOp.addImage(exp);
+        try {
+            convert.run(convertOp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IM4JavaException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
