4750b51d515c8e4fd24007b3f2c67b8954b570a3
Gajendra Sahu <gajendra.sahu@bankersaccuity.com>
2016-09-23 09:03:27 -0500

    added-seperate-classes

---
+++ src/test/java/org/web/kyc/jbehave/RunTestsInParallel.java
@@ -0,0 +1,65 @@
+package org.web.kyc.jbehave;
+
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.web.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+@RunWith(Parallelized.class)
+public class RunTestsInParallel {
+
+    public WebDriverProvider webDriverProvider;
+    public String browser;
+    public String storyName;
+
+    public RunTestsInParallel(String browser, String storyName) {
+        this.browser = browser;
+        this.storyName = storyName;
+    }
+
+        @Before
+        public void setUp () {
+            String URL = "http://localhost:4444/wd/hub";
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability("browserName", browser);
+            System.setProperty("REMOTE_WEBDRIVER_URL", URL);
+            webDriverProvider = new RemoteWebDriverProvider(desiredCapabilities);
+        }
+
+        @After
+        public void tearDown () {
+            webDriverProvider.get().quit();
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data () {
+            Object[][] data = new Object[][]{
+                    //{ "firefox", "+google,+accuity" },
+//                    {"firefox", "+google"},
+//                    {"firefox", "+accuity"},
+                      {"chrome", "+audit"}};
+            return Arrays.asList(data);
+        }
+
+        @Test
+        public void runStories() {
+            Embedder storyEmbedder = new StoryEmbedder(webDriverProvider);
+            List<String> metaFiltersList = newArrayList(storyName.split(","));
+            List<String> storyPaths = new StoryFinder().findPaths(CodeLocations.codeLocationFromPath("./src/main/resources"), "**/*.story", "");
+            storyEmbedder.useMetaFilters(metaFiltersList);
+            storyEmbedder.runStoriesAsPaths(storyPaths);
+        }
+
+}
---
+++ src/test/java/org/web/kyc/jbehave/StoryEmbedder.java
@@ -0,0 +1,70 @@
+package org.web.kyc.jbehave;
+import org.jbehave.asciidoctor.reporter.AsciidoctorStoryReporter;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.embedder.EmbedderControls;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.*;
+import org.jbehave.web.selenium.*;
+import org.web.kyc.jbehave.steps.*;
+
+import java.text.SimpleDateFormat;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+
+public class StoryEmbedder extends Embedder {
+	
+	private WebDriverProvider webDriverProvider;
+	private SeleniumContext context = new SeleniumContext();
+	private ContextView contextView = new LocalFrameContextView().sized(50, 50);
+
+	public StoryEmbedder(WebDriverProvider webDriverProvider) {
+		this.webDriverProvider = webDriverProvider;
+	}
+
+	 @Override
+	    public EmbedderControls embedderControls() {
+	        return new EmbedderControls().doIgnoreFailureInStories(true).doIgnoreFailureInView(true);
+	    }
+	 
+	    @Override
+	    public Configuration configuration() {
+	        Class<? extends StoryEmbedder> embedderClass = this.getClass();
+			return new SeleniumConfiguration()
+					.useSeleniumContext(context)
+					.useWebDriverProvider(webDriverProvider)
+					.useStepMonitor(new SeleniumStepMonitor(contextView, context, new SilentStepMonitor()))
+					.useStoryLoader(new LoadFromClasspath(embedderClass))
+					.useParameterConverters(new ParameterConverters().addConverters(new ParameterConverters.DateConverter(new SimpleDateFormat("yyyy-MM-dd"))))
+					.useStoryReporterBuilder(new StoryReporterBuilder()
+							.withCodeLocation(codeLocationFromClass(embedderClass))
+							.withFormats(org.jbehave.core.reporters.Format.STATS,
+									org.jbehave.core.reporters.Format.CONSOLE,
+									new ScreenShootingHtmlFormat(webDriverProvider),
+									AsciidoctorStoryReporter.ASCIIDOC)
+							.withDefaultFormats()
+							.withFailureTrace(true));
+	    }
+
+	@Override
+	public InjectableStepsFactory stepsFactory() {
+		Configuration configuration = configuration();
+		return new InstanceStepsFactory(configuration,
+                    /* Add all step classes here */
+				new CommonSteps(webDriverProvider),
+				new OwnersSteps(webDriverProvider),
+				new SubsidiariesSteps(webDriverProvider),
+				new EntityDetailsSteps(webDriverProvider),
+				new GroupStructureSteps(webDriverProvider),
+				new AuditSteps(webDriverProvider),
+				new GraphsSteps(webDriverProvider),
+				new GraphControlsSteps(webDriverProvider),
+				new SidePanelSteps(webDriverProvider),
+				new ErrorScreenSteps(webDriverProvider),
+				new BAIntegrationSteps(webDriverProvider),
+				new WebDriverScreenshotOnFailure(webDriverProvider, configuration.storyReporterBuilder()));
+	}
+
+}
